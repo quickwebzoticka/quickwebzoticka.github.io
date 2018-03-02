b23b3cdd261b25f68c212f17cb5fb58a69c513db
quickwebzoticka <36758145+quickwebzoticka@users.noreply.github.com>
2018-03-02 18:42:06 +0300

    Add files via upload

--- js/hide.js
+++ js/hide.js
@@ -1,11 +1,44 @@
-$(document).ready(function() {
+ $(document).click( function(event){
 
-$( function() {
+ 	if( $(event.target).closest("#slider-div").length || $(event.target).closest("#amount").length ) 
+        return;
+      $("#slider-div").css("height", "0");
+      event.stopPropagation();
 
-		$( ".detailsearch" ).click(function(){
-			
-		  $( ".searchmenu" ).css("display:","block");
-		});
+    });
 
-	});
-});
+ $("#amount").click( function(event){
+
+      $("#slider-div").css("height", "40%");
+
+    });
+
+  $(document).click( function(event){
+
+ 	if( $(event.target).closest("#slider-div-1").length || $(event.target).closest("#amount-1").length ) 
+        return;
+      $("#slider-div-1").css("height", "0");
+      event.stopPropagation();
+
+    });
+
+ $("#amount-1").click( function(event){
+
+      $("#slider-div-1").css("height", "40%");
+
+    });
+
+  $(document).click( function(event){
+
+ 	if( $(event.target).closest("#slider-div-2").length || $(event.target).closest("#amount-2").length ) 
+        return;
+      $("#slider-div-2").css("height", "0");
+      event.stopPropagation();
+
+    });
+
+ $("#amount-2").click( function(event){
+
+      $("#slider-div-2").css("height", "40%");
+
+    });
