0b88dced3b902838a3b34906f09470f80f05c6a6
quickwebzoticka <36758145+quickwebzoticka@users.noreply.github.com>
2018-10-02 17:43:29 +0300

    Add files via upload

---
+++ svai/js/myscripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('.table__main__nav').on('click', '.li_nav', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.table__main__active').find('.table').removeClass('table__active').eq($(this).index()).addClass('table__active');
+    });
+
+    $('.table__head').on('click', '.li_nav_main', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.table').find('.table__main').removeClass('table__main__active').eq($(this).index()).addClass('table__main__active');
+    });
+});
