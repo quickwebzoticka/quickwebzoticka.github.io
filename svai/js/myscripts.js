3b196c898bc4426c31f72148da26ba2242555406
quickwebzoticka <36758145+quickwebzoticka@users.noreply.github.com>
2018-10-11 15:23:23 +0300

    Add files via upload

--- svai/js/myscripts.js
+++ svai/js/myscripts.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-	$('.table__main__nav').on('click', '.li_nav:not("active")', function() {
+
+    $('.category-item__table__main__nav').on('click', '.li_nav:not(".active")', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('.category-item')
+            .find('.table')
+            .removeClass('table__active')
+            .css('display', 'none')
+            .eq($(this).index())
+            .slideDown('slow', function() {
+                $(this).addClass('table__active');
+            });
+    });
+
+	$('.table__main__nav').on('click', '.li_nav:not(".active")', function() {
         $(this)
             .addClass('active').siblings().removeClass('active')
             .closest('div.table__main__active')
@@ -10,7 +24,6 @@ $(document).ready(function() {
             .slideDown('slow', function() {
                 $(this).addClass('table__active');
             });
-            
     });
 
     $('.table__head').on('click', '.li_nav_main', function() {
@@ -21,5 +34,15 @@ $(document).ready(function() {
             .removeClass('table__main__active')
             .eq($(this).index())
             .addClass('table__main__active');
+    });
+
+    $('.flex-wrapper').on('click', '.category', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.table__main')
+            .find('.category-item')
+            .removeClass('category-item__active')
+            .eq($(this).index())
+            .addClass('category-item__active');
     });
 });
