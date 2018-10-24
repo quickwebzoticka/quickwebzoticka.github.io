bff813fa36c47725f9f8098fe749fd45acaedda9
quickwebzoticka <36758145+quickwebzoticka@users.noreply.github.com>
2018-10-24 19:03:04 +0300

    Add files via upload

--- dentmir-module/js/myscripts.js
+++ dentmir-module/js/myscripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	$(document).on('click', '.service__title', function(event) {
-	    $(this).siblings('.service__desc').slideToggle('fast');
+	// $(document).on('click', '.service__title', function(event) {
+	//     $(this).siblings('.service__desc').slideToggle('fast');
 
-	});
+	// });
 });
