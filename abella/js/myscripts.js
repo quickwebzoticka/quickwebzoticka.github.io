3b37ed91147447494352d21eefc1d7bfdb699d9a
quickwebzoticka <36758145+quickwebzoticka@users.noreply.github.com>
2018-11-12 18:52:07 +0300

    Add files via upload

--- abella/js/myscripts.js
+++ abella/js/myscripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
+	AOS.init({
+		once: true
+	});
 
+	var slider = $('.seven-screen__gallery');
+
+	slider.slick({
+		arrows: false
+	});
+    $(document).on('click', '.seven-screen__gallery__prev', function (e) {
+        slider.slick('slickPrev');
+        e.preventDefault();
+    });
+    $(document).on('click', '.seven-screen__gallery__next', function (e) {
+        slider.slick('slickNext');
+        e.preventDefault();
+    });
 });
