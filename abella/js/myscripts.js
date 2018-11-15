590105bd1ac43a5635ab23b623d7554fdeeefea8
quickwebzoticka <36758145+quickwebzoticka@users.noreply.github.com>
2018-11-15 18:02:33 +0300

    Add files via upload

--- abella/js/myscripts.js
+++ abella/js/myscripts.js
@@ -10,7 +10,15 @@ $(window).on('load', function (){
 });
 
 $(document).ready(function() {
-	var slider = $('.seven-screen__gallery');
+	var slider = $('.seven-screen__gallery'),
+		windowWidth = $(window).width();
+
+	$(window).resize(function() {
+		windowWidth = $(window).width();
+		console.log(windowWidth);
+		return windowWidth;
+	});
+	
 
 	slider.slick({
 		arrows: false
@@ -22,5 +30,34 @@ $(document).ready(function() {
     $(document).on('click', '.seven-screen__gallery__next', function (e) {
         slider.slick('slickNext');
         e.preventDefault();
+    });
+    $(document).on('click', '.header__menu', function() {
+	    if (windowWidth >= 768) {
+			
+	    }
+	    $('.menu-big').animate({'width': '100vw'}, 'slow');
+    });
+    $(document).on('click', '.menu-big__close', function() {
+	    $('.menu-big').animate({'width': '0vw'}, 'slow');
+    });
+    $(document).on('click', '.counter-minus', function() {
+    	var a = $('.count').val();
+
+    	if (a <= 1) {
+    		return;
+    	}
+    	a--;
+
+    	$('.count').val(a);
+    });
+    $(document).on('click', '.counter-plus', function() {
+    	var a = $('.count').val();
+
+    	if (a > 6000) {
+    		return;
+    	}
+    	a++;
+
+    	$('.count').val(a);
     });
 });
